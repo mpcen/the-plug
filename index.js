d8399e1e3494c878ce73a8ea58d6fad796c0c52a
Manny Martinez <vccengineers12@gmail.com>
2019-03-07 23:45:24 -0800

    updated

--- index.js
+++ index.js
@@ -8,33 +8,28 @@ const PORT = process.env.PORT || 5000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.post('/test', (req, res) => {
-	console.log('test hit');
-
-	res.send({ msg: 'ok' });
-});
-
 app.post('/', (req, res) => {
-	console.log('Got:', req.body);
-	
-	const botResponse = 'yo my realest niggas ever';
-	const options = {
-		method: 'post',
-		url: 'https://api.groupme.com/v3/bots/post',
-		data: {
-			'bot_id': BOT_ID,
-			'text': botResponse
-		}
-	};
+	if(req.body.sender_type !== 'bot') {
+		const botResponse = `${req.body.name} said: ${req.body.text}`;
+		const options = {
+			method: 'post',
+			url: 'https://api.groupme.com/v3/bots/post',
+			data: {
+				'bot_id': BOT_ID,
+				'text': botResponse
+			}
+		};
 
-	// axios(options)
-	// 	.then(response => {
-	// 		res.send({ msg: 'ok' });
-	// 	})
-	// 	.catch(err => {
-	// 		res.sendStatus(500);
-	// 	});
-	res.send({ msg: 'ok' });
+		axios(options)
+			.then(response => {
+				res.send({ msg: 'ok' });
+			})
+			.catch(err => {
+				res.sendStatus(500);
+			});
+	} else {
+		res.sendStatus(200);
+	}
 });
 
 app.listen(PORT, () => {
