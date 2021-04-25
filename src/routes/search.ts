b267a01d04bb26f558b355b9dfba52b2da362bf3
manny martinez <vccengineers12@gmail.com>
2021-04-24 22:03:13 -0700

    Removing logging

--- src/routes/search.ts
+++ src/routes/search.ts
@@ -8,8 +8,6 @@ const dbClient = new MongooseClient();
 router.post('/message', async (req, res) => {
     const { text } = req.body;
 
-    console.log('calling w', text);
-
     const results = await dbClient.getMessages(text);
 
     res.send(results);
